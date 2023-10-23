4895ddfcf126bc4cda4e5c0d310331b4d7679338
ikustov <ikustov@team.amocrm.com>
2023-10-23 04:09:10 +0400

    [add] todo for preview

--- src/layout/Desktop/Desktop.tsx
+++ src/layout/Desktop/Desktop.tsx
@@ -1,10 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { files } from './Desktop.files';
 
 import { File } from './File/File';
 
 export const Desktop: FC = () => {
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      // TODO: open preview by press Space
+      console.log(e);
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
   return <div className='flex-1 p-4 space-y-2'>
     {files.map((file) => <File
       key={file.name}
